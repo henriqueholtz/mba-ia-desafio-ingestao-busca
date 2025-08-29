554ac321508ca76a6a447899aa02ad944a0b6660
Henrique Holtz <henrique_holtz@hotmail.com>
2025-08-29 20:25:33 -0300

    feat: Implementing the ingestion script

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# Desafio MBA Engenharia de Software com IA - Full Cycle
+# Tech challenge for "MBA Engenharia de Software com IA - Full Cycle"
 
-Descreva abaixo como executar a sua solução.
+This is a python project to ingest a PDF into a vector database (postgres + PGVector). You can search using a CLI script.
+
+## Requirements
+
+- Python
+- Docker
+- Gemini API Key
+
+## Setup and run
+
+To setup and run the project easily you will need to follow these steps:
+
+### 1. Environment setup
+
+- Create and activate a virtual environment: `python3 -m venv venv && source venv/Scripts/activate`
+- Create your `.env` file based on `.env.example`: `cp .env.example .env`
+- Setup your Gemini credentials on `.env`
+- Spin up the postgres + pgvector: `docker compose up -d`
+
+### 2. Ingest the PDF into the
+
+```
+python src/ingest.py
+```
--- src/ingest.py
+++ src/ingest.py
@@ -1,12 +1,62 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
-PDF_PATH = os.getenv("PDF_PATH")
+load_dotenv()
+for k in ("LLM_API_KEY", "DATABASE_URL", "EMBEDDING_MODEL", "PG_VECTOR_COLLECTION_NAME"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
 
 def ingest_pdf():
-    pass
+    current_dir = Path(__file__).parent.parent  # Go up one level to the root directory
+    pdf_path = current_dir / "document.pdf"
+
+    print(f"Loading PDF from: {pdf_path}")
+    docs = PyPDFLoader(str(pdf_path)).load()
+    print(f"Loaded {len(docs)} pages from PDF")
+
+    print("Splitting documents into chunks...")
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=150, add_start_index=False).split_documents(docs)
+    if not splits:
+        print("There is no content to split")
+        raise SystemExit(0)
+
+    print("Enriching document metadata...")
+    enrichedDocs = [
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
+    ]
+
+
+    print("Generating embeddings with its document IDs...")
+    ids = [f"doc-{i}" for i in range(len(enrichedDocs))]
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model=os.getenv("EMBEDDING_MODEL","models/embedding-001"),
+        google_api_key=os.getenv("LLM_API_KEY")
+    )
+
+    print("Storing embeddings in PostgreSQL...")
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    print(f"Adding {len(enrichedDocs)} documents to the vector store...")
+    store.add_documents(documents=enrichedDocs, ids=ids)
+    print("Documents successfully added to the vector store!")
 
 
 if __name__ == "__main__":
