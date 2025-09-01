96a9970eb7e64d0db8cc4f0cb5dd3a214fb902b5
Henrique Holtz <henrique_holtz@hotmail.com>
2025-09-01 06:44:37 -0300

    feat: Searching the user input on vector database (postgres + pgvector)

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,18 @@
 from search import search_prompt
 
 def main():
-    chain = search_prompt()
+
+    print("Faça sua pergunta:")
+    user_input = input()
+    print("Você perguntou:", user_input)
+    
+    chain = search_prompt(user_input)
 
     if not chain:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
-    
-    pass
+    # response = chain.run(user_input)
+    # print("Resposta:", response)
 
 if __name__ == "__main__":
     main()
--- src/search.py
+++ src/search.py
@@ -1,3 +1,15 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
+
+load_dotenv()
+for k in ("LLM_API_KEY", "DATABASE_URL", "EMBEDDING_MODEL", "PG_VECTOR_COLLECTION_NAME"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -25,5 +37,26 @@ PERGUNTA DO USUÁRIO:
 RESPONDA A "PERGUNTA DO USUÁRIO"
 """
 
-def search_prompt():
-    pass
+def search_prompt(user_input: str):
+  print("Iniciando busca...")
+  embeddings = GoogleGenerativeAIEmbeddings(
+        model=os.getenv("EMBEDDING_MODEL","models/embedding-001"),
+        google_api_key=os.getenv("LLM_API_KEY")
+  )
+
+  print("Conectando ao banco de dados...")
+  store = PGVector(
+      embeddings=embeddings,
+      collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+      connection=os.getenv("DATABASE_URL"),
+      use_jsonb=True,
+  )
+
+  print("Buscando resultados...")
+  results = store.similarity_search_with_score(user_input, k=10)
+
+  for i, (doc, score) in enumerate(results, start=1):
+      print(f"Resultado {i}: {doc.page_content} (Score: {score})\n")
+      print("-"*75)
+  
+  # return results
