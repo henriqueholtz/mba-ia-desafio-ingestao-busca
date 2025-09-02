214f2ad10a5204ae3499a23195bdc533e76e85af
Henrique Holtz <henrique_holtz@hotmail.com>
2025-09-02 20:35:29 -0300

    feat: Adding loop to ask new questions without need to restart the script

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,17 @@
 from search import search_prompt
 
 def main():
-    user_input = input("Faça sua pergunta: ")
+    while True:
+        if user_input := input("Faça a sua pergunta: "):
+            answer_gemini = search_prompt(user_input)
+            print("-"*75, "\n")
+            print("PERGUNTA: " + user_input)
+            print("RESPOSTA: " + answer_gemini.content)
+            print("-"*75, "\n")
+        else:
+            print("Chat encerrado!")
+            break
 
-    answer_gemini = search_prompt(user_input)
-
-    print("-"*75, "\n")
-    print("PERGUNTA: " + user_input)
-    print("RESPOSTA: " + answer_gemini.content)
 
 if __name__ == "__main__":
     main()
